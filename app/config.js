3686976b8e0983791f42a0ad1fa6a5640d7c5db4
Rebecca Crow <rdcrow529@gmail.com>
2018-10-31 01:31:33 -0400

    added user.model.js

--- app/config.js
+++ app/config.js
@@ -15,9 +15,9 @@ module.exports = {
         NOT_FOUND: 404,
         INTERNAL_SERVER_ERROR: 500
     },
-    MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/just-jotit',
+    MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/tms',
     TEST_MONGO_URL:
-    process.env.TEST_MONGO_URL || 'mongodb://localhost:27017/test-just-jotit',
+    process.env.TEST_MONGO_URL || 'mongodb://localhost:27017/test-tms',
     JWT_SECRET: process.env.JWT_SECRET || 'default',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 };
